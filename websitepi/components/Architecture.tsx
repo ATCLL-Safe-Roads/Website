0b72bd138b7f0054107ae55ea39ec4d3b9d93006
GoncaloSilva25 <goncassilva02@gmail.com>
2023-03-21 09:35:16 +0000

    added Architecture diagram

--- websitepi/components/Architecture.tsx
+++ websitepi/components/Architecture.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 type Props = {};
 
-export default function Architecture({}: Props) {
+export default function Architecture({ }: Props) {
   return (
     <div className="h-screen flex items-center justify-center text-center overflow-hidden text-7xl">
-      <p style={{ fontSize: 25, padding: 10 }}>Futuramente..</p>
+      <img src="diagram.png" alt="Architecture" className="w-3/4" />
     </div>
   );
 }
